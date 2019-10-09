91b13f989a863cfb6abd3f950c7c047ee76b6cdb
Taylor <stein0209@gmail.com>
2019-10-09 16:48:40 -0600

    adds first and last name fields to db

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_10_09_193423) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
