8559df9cb208ca6741cbdce376d2ac22d5e41e4d
Taylor <stein0209@gmail.com>
2019-10-13 21:23:14 -0600

    fixes create waste controller

--- app/controllers/wastes_controller.rb
+++ app/controllers/wastes_controller.rb
@@ -12,6 +12,21 @@ class WastesController < ApplicationController
     end
 
     def create
-        
+        @food_conversion_rate = Food.find_by(id: params[:id]).conversion_rate
+        @quantity = params[:quantity].to_f
+        @cost = @quantity * @food_conversion_rate
+        @waste = current_user.wastes.new(waste_params)
+        @waste.cost = @cost
+        @waste.save
+
+        # byebug
+
+        render json: @waste
+    end
+
+    private 
+
+    def waste_params
+        params.permit(:food_name, :expiration_date, :quantity, :quantity_unit, :value, :food_category, :id, :disposal_method, :disposal_reason)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :foods
+    has_many :wastes
     
     has_secure_password
     validates :username, uniqueness: { case_sensative: false }
